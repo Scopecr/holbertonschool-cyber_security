b46410f0b4ccaba7aa4a4aa76c4610556975d1e6
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2025-01-03 14:38:35 -0400

    Refactor login script to require sudo for displaying last login sessions

--- linux_security/0x00_linux_security_basics/0-login.sh
+++ linux_security/0x00_linux_security_basics/0-login.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-# Check if the script is run as root or with sudo
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root or use sudo."
-  exit 1
-fi
-
-# Display the last 5 login sessions
-last -n 5
+sudo last -n 5 | head -n 5
