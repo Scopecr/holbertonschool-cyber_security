d617b59f6aeb0dbda414d992ba1712fa633d8d90
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2025-02-26 08:11:33 -0400

    Refactor SSH audit script to improve filtering and output handling

--- linux_security/0x03_protocols_servers/1-audit.sh
+++ linux_security/0x03_protocols_servers/1-audit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -vE '^(#|$|Port 22|Protocol 2|PermitRootLogin|PasswordAuthentication|PubkeyAuthentication|UsePAM)' /etc/ssh/sshd_config > sshd_nonstandard.txt
+grep -v "ˆ#" /etc/ssh/sshd_config | grep -Ev "ˆ$"  | awk 'NF' | snort
