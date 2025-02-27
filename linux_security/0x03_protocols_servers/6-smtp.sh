4424b7d4b55ae29c5146ad90ba8367006516b1fe
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2025-02-26 20:54:19 -0400

    Update SMTP script to check for SMTP server availability based on TLS security level

--- linux_security/0x03_protocols_servers/6-smtp.sh
+++ linux_security/0x03_protocols_servers/6-smtp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo | nc -v -w 3 $1 25 2>&1 | grep -q "^220" && echo "SMTP server available" || echo "SMTP server unavailable"
+echo | nc -v -w 3 $1 25 2>&1 | grep -q "^smtpd_tls_security_level" && echo "SMTP server available" || echo "SMTP server unavailable"
