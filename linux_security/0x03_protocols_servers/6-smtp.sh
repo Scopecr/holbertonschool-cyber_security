d6b51cf3e329695b6a7bcaf9e0861b44396554fa
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2025-02-27 08:09:45 -0400

    Update SMTP script to provide additional guidance on STARTTLS configuration

--- linux_security/0x03_protocols_servers/6-smtp.sh
+++ linux_security/0x03_protocols_servers/6-smtp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-openssl s_client -connect $1:25 -starttls smtp </dev/null 2>/dev/null | grep -q "STARTTLS" || echo "STARTTLS not configured"
+openssl s_client -connect $1:25 -starttls smtp </dev/null 2>/dev/null | grep -q "STARTTLS" || echo "STARTTLS not configured - check smtpd_tls_security_level"
