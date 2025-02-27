58c7da64ff2a269ca60cf86086cf767ad3541ee9
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2025-02-26 20:59:23 -0400

    Update SMTP script output message to reflect STARTTLS server configuration status

--- linux_security/0x03_protocols_servers/6-smtp.sh
+++ linux_security/0x03_protocols_servers/6-smtp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo | nc -v -w 3 $1 25 2>&1 | grep -q "^smtpd_tls_security_level" /etc/postfix/main.cf && echo "SMTP server configured" || echo "SMTP server not configured"
+echo | nc -v -w 3 $1 25 2>&1 | grep -q "^smtpd_tls_security_level" /etc/postfix/main.cf && echo "STARTTLS server configured" || echo "STARTTLS server not configured"
