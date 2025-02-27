c9b251730518b0ab06e1f0d1dd1d3179d0a88929
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2025-02-26 20:52:52 -0400

    Update SMTP script to check for SMTP server availability instead of STARTTLS configuration

--- linux_security/0x03_protocols_servers/6-smtp.sh
+++ linux_security/0x03_protocols_servers/6-smtp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo | nc -v -w 3 $1 25 2>&1 | grep -q "STARTTLS" && echo "STARTTLS configured" || echo "STARTTLS not configured"
+echo | nc -v -w 3 $1 25 2>&1 | grep -q "^220" && echo "SMTP server available" || echo "SMTP server unavailable"
