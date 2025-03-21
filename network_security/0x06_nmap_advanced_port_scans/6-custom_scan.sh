fb2cdfb159589b8f3ed543a365d125da0a79109d
Scopecr <wjrcarrion@gmail.com>
2025-03-21 12:44:50 -0400

    6

--- network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
+++ network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo nmap -oN custom_scan.txt>/dev/null -scanflags URGACKPSHRSTYNFIN -p $2 -reason $1 2&>1
+sudo nmap -oN custom_scan.txt>/dev/null -scanflags URGACKPSHRSTYNFIN -p $2 -reason $1 2>&1
