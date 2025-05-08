37d5d8c3cf75f5a8740891d7e2ef4b3cefff1769
Scopecr <wjrcarrion@gmail.com>
2025-05-08 07:24:28 -0400

    removed comments

--- scripting_cyber/0x00-ruby_scripting/1-hello_world_class.rb
+++ scripting_cyber/0x00-ruby_scripting/1-hello_world_class.rb
@@ -1,13 +1,9 @@
 #!/usr/bin/env ruby
-
-# difine class
 class HelloWorld
-  #variable message
   def initialize
-    @message = "Hello World!"
+    @message = "Hello, World!"
   end
 
-#define method
   def print_hello
     puts @message
   end
