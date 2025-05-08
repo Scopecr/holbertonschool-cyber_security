87db3cf157cefbb56698cb212b514ac885b2f5ab
Scopecr <wjrcarrion@gmail.com>
2025-05-07 22:02:01 -0400

    1-hello world class

---
+++ scripting_cyber/0x00-ruby_scripting/1-hello_world_class.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+# difine class
+class HelloWorld
+  #variable message
+  message = "Hello World"
+
+  #define method
+  def print_hello(message)
+    puts "#{message}"
+  end
+end
